74054997c700ee6db12158fe0fe340eaeeeea897
Daniel Supernault <danielsupernault@gmail.com>
2012-03-10 12:53:34 -0700

    New layout

--- index.php
+++ index.php
@@ -1,35 +1,31 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
-    <title>Project Meshnet</title>
-    <meta name="description" content="Project Meshnet aims to establish a decentralized mesh network with CJDNS.">
+    <title>project:meshnet</title>
+    <meta name="description" content="">
     <meta name="author" content="derp">
-
-    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+   <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-
     <!-- Le styles -->
-    <link href="./css/app.css" rel="stylesheet">
+    <link href="http://cdn.projectmeshnet.net/org/css/app.css" rel="stylesheet">
     <style type="text/css">
-      /* Override some defaults */
       html, body {
         background-color: #eee;
       }
-      body {
-        padding-top: 60px; /* 40px to make the container go all the way to the bottom of the topbar */
+      section {
+      padding-top: 60px;
       }
-      .container > footer p {
+	  section > .row {
+ 	  margin-bottom: 10px;
+	  }
+    .container > footer p {
         text-align: center; /* center align it with the container */
       }
-     
       /* The white background content wrapper */
-      .container {
-	   padding: 10px 0;
-	 }
-      	 .content {
+      .container > .content {
         background-color: #fff;
         padding: 20px;
         margin: 0 -20px; /* negative indent the amount of the padding to maintain the grid system */
@@ -41,63 +37,92 @@
                 box-shadow: 0 1px 2px rgba(0,0,0,.15);
       }
 
-     .content .span10,
-      .content .span4 {
-        min-height: 500px;
+      /* Page header tweaks */
+      .page-header {
+        background-color: #f5f5f5;
+        padding: 0px 20px 10px;
       }
-      /* Give a quick and non-cross-browser friendly divider */
+
+           /* Give a quick and non-cross-browser friendly divider */
       .content .span4 {
         margin-left: 0;
         padding-left: 19px;
         border-left: 1px solid #eee;
       }
 
-      .navbar .btn {
+      .topbar .btn {
         border: 0;
       }
-	    </style>
-  </head>
 
+    </style>﻿<link rel="shortcut icon" href="images/favicon.ico">
+   <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+      </head>
   <body>
-
-    <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
+  <!---nav include--->
+﻿<div class="topbar" data-dropdown="dropdown">
+      <div class="fill">
         <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="/">Project Meshnet</a>
-          <div class="nav-collapse">
-            <ul class="nav">
-              <li class="active"><a href="/">Home</a></li>
-              <li><a href="#">About</a></li>
-                         </ul>
-                      </div><!--/.nav-collapse -->
+          <a class="brand" href="#"><img src="http://cdn.projectmeshnet.net/org/images/meshsmall.png?v=445"></a>
+          <ul class="nav">
+            <li><a href="./">Home</a></li>
+            <li><a href="./about.php">About</a></li>
+              <li class="dropdown">
+              <a href="#" class="dropdown-toggle">Get Involved</a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Getting Started</a></li>
+                 <li class="divider"></li>
+                 <li><a href="#">MeshMaps</a></li>
+                 <li><a href="#">MeshLocal</a></li>
+			              </ul>
+                          </li>
+			<li><a href="#">Wiki</a></li>
+			                         </ul>    <ul class="nav secondary-nav">
+                  <a href="#" class="btn small primary"><h6a>Login</h6a></a> </ul>
         </div>
       </div>
+</div>          
+    <header class="jumbotron masthead" id="overview">
+<div class="inner">
+        <div class="container"><br><br>
+          <img src="http://cdn.projectmeshnet.net/org/images/mesh.png?V=148"><br><br><br>
+          <p class="lead">
+            Objective: 
+To create a versatile, decentralized network built on secure protocols for routing <br>traffic over private mesh or public internetworks 
+independent of a central supporting infrastructure.           </p>
+       <br><br><br><br> </div><!-- /container -->
+      </div></header>
     <div class="container">
+     <div class="content"> <div class="page-header">
+          <br><p><a href="/"> home /</a></p>
+        </div>
 
-      <div class="content">
-                <div class="row">
-           <div class="span14">
-           <div class="alert alert-error">
- 			Website under construction. Thank you for your patience.
-				</div>
-                           <div class="hero-unit">
-        <h1>Welcome!</h1>
-             </div>
-
-    </div>
-     
-	</div></div>
-      <footer>
-        
+      <div class="row">
+        <div class="span-one-third">
+   <div style="text-align:center"><h2></h2>
+<p><img src="http://cdn.projectmeshnet.net/org/images/icon-07.png" alt="projects icon"></p><br>
+<p><b>[samuel l jackson lorum ipsum :)] </b>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, 
+find out where my goddamn money is. </p>
+          <p><a class="btn disabled" href="#">coming soon &raquo;</a></p></div>
+        </div>
+        <div class="span-one-third">
+          <div style="text-align:center"><h2>meshlocal<span class="label notice">beta</span></h2>
+<p> <img src="http://cdn.projectmeshnet.net/org/images/icon-16.png" alt="meshlocal"></p><br>
+         <p> She gonna tell me too. Hey, look at me when I'm talking to you, <b>motherfucker</b>. <em>You listen:</em> we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?</p>
+          <p><a class="btn primary" href="#">go &raquo;</a></p>
+       </div></div>
+        <div class="span-one-third">
+          <div style="text-align:center"><h2>meshwiki</h2>
+<p> <img src="http://cdn.projectmeshnet.net/org/images/icon-18.png" alt=""></p><br>
+         <p> She gonna tell me too. Hey, look at me when I'm talking to you, <b>motherfucker</b>. <em>You listen:</em> we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?</p>
+         <p><a class="btn primary" href="#">go &raquo;</a></p>
+       </div> </div>
+      </div></div>
+		<footer>
+        <p>copyright &copy; 2012 projectmeshnet.org. all rights reserved.</p><div style="float:right"><a href="./contact_us.php">contact us</a> | <a href="./developers.php">developers</a> | <a href="./privacy.php">privacy</a> | <a href="./sitemap.php">sitemap</a></div>
       </footer>
-
-    
-
-    </div> <!-- /container -->
-      </body>
+    ﻿ <script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
+     <script src="http://twitter.github.com/bootstrap/1.4.0/bootstrap-dropdown.js"></script>
+      </div></body>
 </html>
