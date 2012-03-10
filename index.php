c0710999a28c1d20da0e209e576c29e6b9e07ff9
Daniel Supernault <danielsupernault@gmail.com>
2012-03-10 12:38:28 -0700

    basic template

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Project Meshnet</title>
+    <meta name="description" content="Project Meshnet aims to establish a decentralized mesh network with CJDNS.">
+    <meta name="author" content="derp">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+    <link href="./css/app.css" rel="stylesheet">
+    <style type="text/css">
+      /* Override some defaults */
+      html, body {
+        background-color: #eee;
+      }
+      body {
+        padding-top: 60px; /* 40px to make the container go all the way to the bottom of the topbar */
+      }
+      .container > footer p {
+        text-align: center; /* center align it with the container */
+      }
+     
+      /* The white background content wrapper */
+      .container {
+	   padding: 10px 0;
+	 }
+      	 .content {
+        background-color: #fff;
+        padding: 20px;
+        margin: 0 -20px; /* negative indent the amount of the padding to maintain the grid system */
+        -webkit-border-radius: 0 0 6px 6px;
+           -moz-border-radius: 0 0 6px 6px;
+                border-radius: 0 0 6px 6px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.15);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.15);
+                box-shadow: 0 1px 2px rgba(0,0,0,.15);
+      }
+
+     .content .span10,
+      .content .span4 {
+        min-height: 500px;
+      }
+      /* Give a quick and non-cross-browser friendly divider */
+      .content .span4 {
+        margin-left: 0;
+        padding-left: 19px;
+        border-left: 1px solid #eee;
+      }
+
+      .navbar .btn {
+        border: 0;
+      }
+	    </style>
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+          <a class="brand" href="/">Project Meshnet</a>
+          <div class="nav-collapse">
+            <ul class="nav">
+              <li class="active"><a href="/">Home</a></li>
+              <li><a href="#">About</a></li>
+                         </ul>
+                      </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    <div class="container">
+
+      <div class="content">
+                <div class="row">
+           <div class="span14">
+           <div class="alert alert-error">
+ 			Website under construction. Thank you for your patience.
+				</div>
+                           <div class="hero-unit">
+        <h1>Welcome!</h1>
+             </div>
+
+    </div>
+     
+	</div></div>
+      <footer>
+        
+      </footer>
+
+    
+
+    </div> <!-- /container -->
+      </body>
+</html>
